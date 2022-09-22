c13f41f12ea9d16cc8b962d950ba1059c1c5b346
talaxasy <talaxasy.or.lev@gmail.com>
2022-09-22 08:31:07 +0300

    recreate content,js

--- utils/content.js
+++ utils/content.js
@@ -1,48 +1,77 @@
-import frontmatter from "front-matter";
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import glob from 'glob';
+import frontmatter from 'front-matter';
 
-const sbConfig = {
-  pagesDir: "content/pages",
-  dataDir: "content/data",
-};
+export const sbConfig = yaml.load(fs.readFileSync('./stackbit.yaml', 'utf8'));
+if (!sbConfig.pagesDir || !sbConfig.dataDir)
+  throw new Error('Invalid Stackbit config file');
 
-/*
-Loading local content from files: while in dev you could simply read files, for production you should:
-(a) include needed files as server-side assets (see nitro.config.ts)
-(b) read the file content via useStorage().getItem()
-See: https://nitro.unjs.io/guide/introduction/assets
-*/
+export const siteConfigFile = sbConfig.dataDir + '/config.json';
+export const siteStyleFile = sbConfig.dataDir + '/style.json';
 
-async function readContent(itemName) {
-  const storageKey = `assets/${itemName}`;
-  const rawContent = await useStorage().getItem(storageKey);
-  console.log(itemName, rawContent);
+const supportedFileTypes = ['md', 'json'];
 
+function contentFilesInPath(dir) {
+  const globPattern = `${dir}/**/*.{${supportedFileTypes.join(',')}}`;
+  return glob.sync(globPattern);
+}
+
+async function readContent(file) {
+  const rawContent = fs.readFileSync(file, 'utf8');
   let content = null;
-  const extension = itemName.split(".").pop();
-  switch (extension) {
-    case "md":
+  switch (path.extname(file).substring(1)) {
+    case 'md':
       const parsedMd = frontmatter(rawContent);
       content = {
         ...parsedMd.attributes,
         body: parsedMd.body,
       };
       break;
-    case "json":
+    case 'json':
       content = JSON.parse(rawContent);
       break;
     default:
-      throw Error(`Unhandled item type: ${itemName}`);
+      throw Error(`Unhandled file type: ${file}`);
   }
 
-  content.__id = itemName;
+  content.__id = file;
+  content.__url = fileToUrl(file);
   return content;
+}
+
+function fileToUrl(file) {
+  if (!file.startsWith(sbConfig.pagesDir)) return null;
+
+  let url = file.slice(sbConfig.pagesDir.length);
+  url = url.split('.')[0];
+  if (url.endsWith('/index')) {
+    url = url.slice(0, -6) || '/';
+  }
+  return url;
+}
+
+function urlToFilePairs() {
+  const pageFiles = contentFilesInPath(sbConfig.pagesDir);
+  return pageFiles.map((file) => [fileToUrl(file), file]);
 }
 
 export async function urlToContent(url) {
-  let itemName = sbConfig.pagesDir + url;
-  if (itemName.endsWith("/")) itemName += "index";
-  itemName += ".md";
-
-  const content = await readContent(itemName);
+  const urlToFile = Object.fromEntries(urlToFilePairs());
+  const file = urlToFile[url];
+  const content = await readContent(file);
   return content;
 }
+
+export async function pagesByType(contentType) {
+  let result = {};
+  for (const [url, file] of urlToFilePairs()) {
+    const content = await readContent(file);
+    if (content.type === contentType) result[url] = content;
+  }
+  return result;
+}
+
+export const siteConfig = async () => readContent(siteConfigFile)
+export const siteStyle = async () => readContent(siteStyleFile)
